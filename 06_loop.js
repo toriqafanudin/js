b2cea69ff7b107a972154568c67c05df806af2c7
toriqafanudin <142080615+toriqafanudin@users.noreply.github.com>
2023-08-22 13:29:32 +0700

    Add files via upload

---
+++ 06_loop.js
@@ -0,0 +1,20 @@
+for(let i = 0; i <= 5; i++){
+	console.log(i);
+}
+
+let myArray = ['Luke', 'Han', 'Chewbacca', 'Leia'];
+for(const arrayItem of myArray){
+	console.log(arrayItem);
+}
+
+let i = 1;
+while(i <= 5){
+	console.log(i);
+	i++;
+}
+
+let j = 25;
+do{
+	console.log(j);
+	j++;
+}while(j <= 30);
